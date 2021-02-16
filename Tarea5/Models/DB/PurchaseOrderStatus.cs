9b27992d163fe6c52dec8196245569baee7a2e58
DannyOzuna <danny20011010@gmail.com>
2021-02-16 16:59:38 -0400

    Consulta 1 realizanda

---
+++ Tarea5/Models/DB/PurchaseOrderStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Tarea5.Models.DB
+{
+    public partial class PurchaseOrderStatus
+    {
+        public PurchaseOrderStatus()
+        {
+            PurchaseOrders = new HashSet<PurchaseOrder>();
+        }
+
+        public int Id { get; set; }
+        public string Status { get; set; }
+
+        public virtual ICollection<PurchaseOrder> PurchaseOrders { get; set; }
+    }
+}
---
+++ Tarea5/Models/DB/String.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Tarea5.Models.DB
+{
+    public partial class String
+    {
+        public int StringId { get; set; }
+        public string StringData { get; set; }
+    }
+}
