e73c9c116ba83659e69bceddb234eb70ec4e7dcc
DannyOzuna <danny20011010@gmail.com>
2021-02-17 00:43:03 -0400

    Inicio de trabajo consulta 3

--- Tarea5/obj/Debug/net5.0/RazorDeclaration/Pages/Consulta.razor.g.cs
+++ Tarea5/obj/Debug/net5.0/RazorDeclaration/Pages/Consulta.razor.g.cs
@@ -13,77 +13,77 @@ namespace Tarea5.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 1 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 2 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 3 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 4 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 5 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 6 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 7 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 8 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 9 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Tarea5;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 10 "C:\Users\danny\Desktop\Programacion-III\tarea5\_Imports.razor"
 using Tarea5.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\danny\Desktop\Programacion-III\Tarea5\Pages\Consulta.razor"
+#line 2 "C:\Users\danny\Desktop\Programacion-III\tarea5\Pages\Consulta.razor"
 using Models.DB;
 
 #line default
@@ -98,7 +98,7 @@ using Models.DB;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 34 "C:\Users\danny\Desktop\Programacion-III\Tarea5\Pages\Consulta.razor"
+#line 34 "C:\Users\danny\Desktop\Programacion-III\tarea5\Pages\Consulta.razor"
       
     //List<Employee> GetEmployees() => new northwindContext().Employees(e => e.FirstName).ToList();
     List<Employee> GetEmployees() => (from d in new northwindContext().Employees
