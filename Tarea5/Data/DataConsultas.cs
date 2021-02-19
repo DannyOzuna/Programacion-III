75dd6c634f0963b9f08e0705c11fb5d87633c8dd
DannyOzuna <danny20011010@gmail.com>
2021-02-19 17:10:54 -0400

    Cambios realizados

--- Tarea5/Data/DataConsultas.cs
+++ Tarea5/Data/DataConsultas.cs
@@ -5,7 +5,18 @@ public class DataConsulta3{
     public int count {get; set;} 
 }
 
+public class DataConsulta4{
+    public string nombre {get; set;}
+    public int cantidad {get; set;}
+}
+
 public class DataConsulta5{
     public string producto {get; set;}
+    public decimal cantidad {get; set;}
+}
+
+public class DataConsulta6{
+    public string producto_codigo {get; set;}
+    public string nombre {get; set;}
     public decimal cantidad {get; set;}
 }
--- Tarea5/obj/Debug/net5.0/RazorDeclaration/Pages/Consulta5.razor.g.cs
+++ Tarea5/obj/Debug/net5.0/RazorDeclaration/Pages/Consulta5.razor.g.cs
@@ -12,90 +12,90 @@ namespace Tarea5.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 1 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 2 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 3 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 4 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 5 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 6 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 7 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 8 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 9 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Tarea5;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\danny\Desktop\Programacion-III\Tarea5\_Imports.razor"
+#line 10 "C:\Users\danny\Desktop\Programacion\Tarea5\_Imports.razor"
 using Tarea5.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\danny\Desktop\Programacion-III\Tarea5\Pages\Consulta5.razor"
+#line 2 "C:\Users\danny\Desktop\Programacion\Tarea5\Pages\Consulta5.razor"
 using Models.DB;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\danny\Desktop\Programacion-III\Tarea5\Pages\Consulta5.razor"
+#line 3 "C:\Users\danny\Desktop\Programacion\Tarea5\Pages\Consulta5.razor"
 using System;
 
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/consulta4")]
+    [Microsoft.AspNetCore.Components.RouteAttribute("/consulta5")]
     public partial class Consulta5 : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
@@ -104,7 +104,7 @@ using System;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 30 "C:\Users\danny\Desktop\Programacion-III\Tarea5\Pages\Consulta5.razor"
+#line 30 "C:\Users\danny\Desktop\Programacion\Tarea5\Pages\Consulta5.razor"
        
   northwindContext postContext = new northwindContext();
   List<DataConsulta5> GetConsulta5() => (from p in postContext.Products
